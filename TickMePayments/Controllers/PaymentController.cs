7cdb3318a92ce71553b898439ebda001911dae77
Pedro Ribeiro <peribei@microsoft.com>
2018-09-08 11:13:16 +0100

    Containerised

---
+++ TickMePayments/Controllers/PaymentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TickMeHelpers;
+
+namespace TickMePayments.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] string value)
+        {
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                var responseError = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("Bad Request")
+                };
+                return responseError;
+            }
+            var paymentData = JsonConvert.DeserializeObject<PaymentData>(value);
+            paymentData = PaymentManagement.ProcessPayment(paymentData);
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(paymentData.ToString())
+            };
+            return response;
+        }
+    }
+}
